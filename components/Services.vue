5f4dcd3fc3b3df4d6983a4edd387a185a3638c4f
tony0g <tony0light@gmail.com>
2025-01-04 00:52:22 -0800

    Changed a NuxtImg to and img

--- components/Services.vue
+++ components/Services.vue
@@ -11,7 +11,7 @@
         <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
           <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
             <div v-for="(feature, index) in features" :key="feature.name" class="flex flex-col">
-              <NuxtImg :src="hoveredIndex === index ? feature.after : feature.before"
+              <img :src="hoveredIndex === index ? feature.after : feature.before"
               @mouseover="handleHover(index)"
               @mouseleave="handleLeave" 
               class="rounded-xl" 
