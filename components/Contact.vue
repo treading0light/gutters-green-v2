7979f596c17980ac2fdecdce5811ae6375998bf0
tony0g <tony0light@gmail.com>
2025-01-03 22:40:53 -0800

    Filled about section with my images cropped to fit components aspect ratio, updated copy, minor style changes

--- components/Contact.vue
+++ components/Contact.vue
@@ -79,7 +79,7 @@
               </div>
             </div>
             <div class="mt-8 flex justify-end">
-              <button type="submit" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send message</button>
+              <button type="submit" class="rounded-md bg-primary px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send message</button>
             </div>
           </div>
         </form>
--- components/Services.vue
+++ components/Services.vue
@@ -4,7 +4,9 @@
         <div class="mx-auto max-w-2xl lg:text-center">
           <h2 class="text-base/7 font-semibold text-indigo-600"></h2>
           <p class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl lg:text-balance">Our Services</p>
-          <p class="mt-6 text-lg/8 text-gray-600">With over 10 years experience in all things gutters, you can trust that all your needs will be met and questions answered.</p>
+          <p class="mt-6 text-2xl/8 text-gray-600">
+            Learn more about our services and how we can help you with your home improvement needs.
+          </p>
         </div>
         <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
           <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
