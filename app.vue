de960cb423eca70cbbe32d5baa98d2d2764a8715
Brave Rose <braverose@Braves-MacBook-Pro.local>
2025-01-24 07:06:39 -0800

    Added meta data

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup>
+useHead({
+  title: 'The Gutters Green',
+  meta: [
+    { name: 'description', content: 'The official website of The Gutters Green LLC'}
+  ]
+})
+</script>
+
 <template>
   <div class="bg-white">
     <MyNav />
