d6143315fdde1a8f365654d36f560584ccd0693a
Tony Green <tony0light@gmail.com>
2025-12-26 11:59:38 -0800

    Added llms.txt and google analytics scripts in the config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,22 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'manifest', href: '/site.webmanifest' },
       ],
+      script: [
+        {    
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-516EPNKBT5',
+          async: true,
+        },
+        {
+          id: 'ga-inline',
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-516EPNKBT5');
+            `,
+        }
+      ]
     },
   },
   site: { 
